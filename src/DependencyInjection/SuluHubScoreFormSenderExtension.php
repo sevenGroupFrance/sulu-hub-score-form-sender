32185c136ec6ffcd30afc0cf925074b71eb1d4eb
Tenreiro Benjamin <47473393+tommysonsylverstone@users.noreply.github.com>
2023-06-28 17:15:42 +0200

    Update SuluHubScoreFormSenderExtension.php

--- src/DependencyInjection/SuluHubScoreFormSenderExtension.php
+++ src/DependencyInjection/SuluHubScoreFormSenderExtension.php
@@ -14,9 +14,13 @@ class SuluHubScoreFormSenderExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $container->setParameter('sulu_hub_score_form_sender.login.id', $config['login']['id']);
-        $container->setParameter('sulu_hub_score_form_sender.login.pwd', $config['login']['pwd']);
-        $container->setParameter('sulu_hub_score_form_sender.forms', $config['forms']);
+        $container->setParameter('sulu_hub_score_form_sender.base_configuration.id', $config['base_configuration']['id']);
+        $container->setParameter('sulu_hub_score_form_sender.base_configuration.pwd', $config['base_configuration']['pwd']);
+        $container->setParameter('sulu_hub_score_form_sender.base_configuration.base_url', $config['base_configuration']['base_url']);
+        $container->setParameter('sulu_hub_score_form_sender.login_url', $config['login_url']);
+        $container->setParameter('sulu_hub_score_form_sender.payload_configuration.campagn_id', $config['payload_configuration']['campagn_id']);
+        $container->setParameter('sulu_hub_score_form_sender.payload_configuration.database_id', $config['payload_configuration']['database_id']);
+        $container->setParameter('sulu_hub_score_form_sender.send_mail_url', $config['send_mail_url']);
         $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.xml');
     }
